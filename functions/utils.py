bddc8712774f6f826ac61a79365057ef219e8aeb
NUST-Machine-Intelligence-Laboratory <58131148+NUST-Machine-Intelligence-Laboratory@users.noreply.github.com>
2021-10-03 16:56:42 +0800

    Add files via upload

---
+++ functions/utils.py
@@ -0,0 +1,24 @@
+from collections import namedtuple
+from string import Template
+import cupy
+import torch
+
+
+Stream = namedtuple('Stream', ['ptr'])
+
+
+def Dtype(t):
+    if isinstance(t, torch.cuda.FloatTensor):
+        return 'float'
+    elif isinstance(t, torch.cuda.DoubleTensor):
+        return 'double'
+
+
+@cupy.util.memoize(for_each_device=True)
+def load_kernel(kernel_name, code, **kwargs):
+    # print("kernel_name",kernel_name)
+    code = Template(code).substitute(**kwargs)
+    # print(code)
+
+    kernel_code = cupy.cuda.compile_with_cache(code)
+    return kernel_code.get_function(kernel_name)
